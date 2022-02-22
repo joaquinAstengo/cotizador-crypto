dc9a3f3cfd443bea218cde460786c1f8bc2ea4b5
Joaquin <joaquin.astengo@gmail.com>
2022-02-22 01:48:05 -0300

    iniciando

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import styled from '@emotion/styled'
+
+const Heading = styled.h1 `
+  font-family: 'Lato', sans-serif;
+  color: #fff;
+`
+
+function App() {
+  
+  return (
+    <Heading>
+
+      <h1>Desde app</h1>
+    </Heading>
+  )
+}
+
+export default App
